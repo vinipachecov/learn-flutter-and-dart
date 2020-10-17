98f7fb62b868284f3d238a3a41d4cd3b12dd0847
Vinicius Vieira <vinipachecov@gmail.com>
2020-10-17 19:11:06 -0300

    feat: filter dummy data through the category id passed from navigation and rendered all items as text values (temp workaround)

--- meals_app/lib/category_meals_screen.dart
+++ meals_app/lib/category_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -12,11 +14,14 @@ class CategoryMealsScreen extends StatelessWidget {
     // extracting data passed through navigation
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
-    // final categoryId = routeArgs['id'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) => meal.categories.contains(categoryId)).toList();
 
     return Scaffold(
       appBar: AppBar(title: Text(categoryTitle)),
-      body: Center(child: Text('The Recipes for the Category!'))
+      body: ListView.builder(itemBuilder: (ctx, index) {
+        return Text(categoryMeals[index].title);
+      }, itemCount: categoryMeals.length,)
     );
   }
 }
