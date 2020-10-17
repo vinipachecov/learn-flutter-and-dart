d83ec1a8e96d5cd2c8662e587d18ad0cc45e9528
Vinicius Vieira <vinipachecov@gmail.com>
2020-10-17 18:29:37 -0300

    chore: added category meals screen two properties to render more accurate information

--- meals_app/lib/category_meals_screen.dart
+++ meals_app/lib/category_meals_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key key}) : super(key: key);
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle); 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('The Recipes')),
+      appBar: AppBar(title: Text(categoryTitle)),
       body: Center(child: Text('The Recipes for the Category!'))
     );
   }
