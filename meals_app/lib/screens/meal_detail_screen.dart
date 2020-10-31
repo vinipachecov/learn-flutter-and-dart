82912280892ebaf88626bebd22ed3d49600e5853
Vinicius Vieira <vinipachecov@gmail.com>
2020-10-31 18:17:11 -0300

    feat: added button to simulate backward navigation

--- meals_app/lib/screens/meal_detail_screen.dart
+++ meals_app/lib/screens/meal_detail_screen.dart
@@ -74,6 +74,11 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.delete),
+          onPressed: () {
+            Navigator.of(context).pop(mealId);
+          }),
     );
   }
 }
