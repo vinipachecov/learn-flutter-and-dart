7b2aebc85cc500b5f7513ce1230215e37f9bd350
Vinicius Vieira <vinipachecov@gmail.com>
2020-10-30 22:07:58 -0300

    feat: incorporated the new custom drawer into the tabs screen

--- meals_app/lib/screens/tabs_screens.dart
+++ meals_app/lib/screens/tabs_screens.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/favorites_screen.dart';
+import 'package:meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -27,6 +28,7 @@ class _TabsScreenState extends State<TabsScreen> {
      */
     return Scaffold(
       appBar: AppBar(title: Text(_pages[_selectedPageIndex]['title'])),
+      drawer: MainDrawer(),
       body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
           onTap: _selectPage,
