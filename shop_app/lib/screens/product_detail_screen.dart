7e62f770d13ccd3c96c301ad7ec6016b2c602561
Vinicius Vieira <vinipachecov@gmail.com>
2020-11-12 21:58:47 -0300

    feat: added new ProductDetailScreen template

---
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static const routeName = '/product-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
+    return Scaffold(
+      appBar: AppBar(title: Text(loadedProduct.title)),
+    );
+  }
+}
