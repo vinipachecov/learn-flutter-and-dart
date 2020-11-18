deddb71540dfd826274096a8f9b91049ec46cfdb
Vinicius Vieira <vinipachecov@gmail.com>
2020-11-17 21:08:19 -0300

    chore: add product details basics

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -11,7 +11,37 @@ class ProductDetailScreen extends StatelessWidget {
     final loadedProduct =
         Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-      appBar: AppBar(title: Text(loadedProduct.title)),
-    );
+        appBar: AppBar(
+          title: Text(loadedProduct.title),
+        ),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              loadedProduct.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            '\$${loadedProduct.price}',
+            style: TextStyle(color: Colors.grey, fontSize: 20),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              loadedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          )
+        ])));
   }
 }
