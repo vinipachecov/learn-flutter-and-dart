c52cd9bf5b12c402a2654244cf0bd2358f66d9f8
Vinicius Vieira <vinipachecov@gmail.com>
2020-11-22 08:42:12 -0300

    feat: register edit screen

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/product_overview_screen.dart';
@@ -37,6 +38,7 @@ class MyApp extends StatelessWidget {
             CartScreen.routeName: (ctx) => CartScreen(),
             OrdersScreen.routeName: (ctx) => OrdersScreen(),
             UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+            EditProductScreen.routeName: (ctx) => EditProductScreen()
           },
         ));
   }
---
+++ shop_app/lib/screens/edit_product_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Edit Product"),
+      ),
+      //  display a form to allow user to enter a title and edit products
+      body: Form(
+        child: ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            TextFormField(
+              decoration: InputDecoration(labelText: "Title"),
+              // appearance of the submit button in the keyboard
+              textInputAction: TextInputAction.next,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
