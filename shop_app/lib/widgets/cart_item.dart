3b8a5fe4dceba3b8287822e5c2626aeb64f08800
Vinicius Vieira <vinipachecov@gmail.com>
2020-11-22 08:29:00 -0300

    feat: add dialog to confirm dismiss actions to delete item

--- shop_app/lib/widgets/cart_item.dart
+++ shop_app/lib/widgets/cart_item.dart
@@ -28,6 +28,28 @@ class CartItem extends StatelessWidget {
           margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
         ),
         direction: DismissDirection.endToStart,
+        confirmDismiss: (direction) {
+          return showDialog(
+              context: context,
+              builder: (ctx) => AlertDialog(
+                    title: Text('Are you sure?'),
+                    content:
+                        Text("Do you want to remove the item from the cart?"),
+                    actions: [
+                      FlatButton(
+                          onPressed: () {
+                            // Forward a value to the return of the dialog
+                            Navigator.of(context).pop(false);
+                          },
+                          child: Text('No')),
+                      FlatButton(
+                          onPressed: () {
+                            Navigator.of(context).pop(true);
+                          },
+                          child: Text('Yes'))
+                    ],
+                  ));
+        },
         onDismissed: (direction) {
           Provider.of<Cart>(context).removeItem(productId);
         },
