eeef0110772e3502a187ab0b2ed4b0fdc812c31b
Vinicius Vieira <vinipachecov@gmail.com>
2020-11-23 09:30:47 -0300

    chore: focus nodes disposal

--- shop_app/lib/screens/edit_product_screen.dart
+++ shop_app/lib/screens/edit_product_screen.dart
@@ -8,6 +8,18 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  /**
+   * When working with focus nodes we have to release them or they will be kept in memory
+   * and cause possible memory leaks.
+   */
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,8 +45,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
               decoration: InputDecoration(labelText: "Price"),
               focusNode: _priceFocusNode,
               // appearance of the submit button in the keyboard
+              onFieldSubmitted: (_) {
+                // Ensure price textfield is focused
+                FocusScope.of(context).requestFocus(_descriptionFocusNode);
+              },
               textInputAction: TextInputAction.next,
               keyboardType: TextInputType.number,
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: "Description"),
+              maxLines: 3,
+              keyboardType: TextInputType.multiline,
+              focusNode: _descriptionFocusNode,
             ),
           ],
         ),
