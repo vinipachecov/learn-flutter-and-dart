2d8049d487b296f329aa563ccd907d13b4896698
Vinicius Vieira <vinipachecov@gmail.com>
2020-11-30 10:06:26 -0300

    chore: keep build context into a variable so when delete fails, the context is not lost as the onPress is now returning a **Future**

--- shop_app/lib/widgets/user_product_item.dart
+++ shop_app/lib/widgets/user_product_item.dart
@@ -12,6 +12,7 @@ class UserProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final scaffold = context;
     return ListTile(
       title: Text(title),
       leading: CircleAvatar(
@@ -30,8 +31,14 @@ class UserProductItem extends StatelessWidget {
                 color: Theme.of(context).primaryColor),
             IconButton(
               icon: Icon(Icons.delete),
-              onPressed: () {
-                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              onPressed: () async {
+                try {
+                  await Provider.of<Products>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (e) {
+                  Scaffold.of(scaffold)
+                      .showSnackBar(SnackBar(content: Text('Deleting failed')));
+                }
               },
               color: Theme.of(context).errorColor,
             )
