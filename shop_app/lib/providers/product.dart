3fab95532adcd90c5b6f08cf92a33bbd30605acf
Vinicius Vieira <vinipachecov@gmail.com>
2020-11-30 20:07:21 -0300

    feat: ensure update Product update is propagated to firebase backend

--- shop_app/lib/providers/product.dart
+++ shop_app/lib/providers/product.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop_app/utils/api.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -7,7 +12,6 @@ class Product with ChangeNotifier {
   final double price;
   final String imageUrl;
   bool isFavorite;
-
   Product(
       {@required this.id,
       @required this.title,
@@ -16,8 +20,28 @@ class Product with ChangeNotifier {
       @required this.imageUrl,
       this.isFavorite = false});
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  void toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
+    final url = Api.url + '/products/$id.json';
+    try {
+      final response =
+          await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+      /**
+           * Patch, PUT and Delete on Firebase don't return errors
+           */
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (e) {
+      _setFavValue(oldStatus);
+      throw e;
+    }
     notifyListeners();
   }
 }
