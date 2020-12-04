69fcba12da862556fb12d64634868b394175ab49
Vinicius Vieira <vinipachecov@gmail.com>
2020-12-04 08:47:14 -0300

    chore: added call to provider signup

--- shop_app/lib/screens/auth_screen.dart
+++ shop_app/lib/screens/auth_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/auth.dart';
 
 enum AuthMode { Signup, Login }
 
@@ -100,7 +102,7 @@ class _AuthCardState extends State<AuthCard> {
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
-  void _submit() {
+  void _submit() async {
     if (!_formKey.currentState.validate()) {
       // Invalid!
       return;
@@ -112,6 +114,8 @@ class _AuthCardState extends State<AuthCard> {
     if (_authMode == AuthMode.Login) {
       // Log user in
     } else {
+      await Provider.of<Auth>(context, listen: false)
+          .signup(_authData['email'], _authData['password']);
       // Sign user up
     }
     setState(() {
