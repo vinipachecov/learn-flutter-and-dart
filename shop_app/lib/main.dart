5cb917b59f5e8b6e6b3c6c5bdd5efec7c710738e
Vinicius Vieira <vinipachecov@gmail.com>
2020-12-04 08:46:57 -0300

    chore: add Auth Provider to MultiProvider

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/providers/auth.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/providers/products.dart';
@@ -18,6 +19,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
         providers: [
+          ChangeNotifierProvider(
+            create: (ctx) => Auth(),
+          ),
           ChangeNotifierProvider(
             create: (ctx) => Products(),
           ),
