1bfbca06154c013ae1bf9116bd301163125ec484
Vinicius Vieira <vinipachecov@gmail.com>
2020-12-04 09:14:36 -0300

    feat: catch error on signingup

--- shop_app/lib/providers/auth.dart
+++ shop_app/lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 import 'package:shop_app/utils/api.dart';
 
 class Auth with ChangeNotifier {
@@ -9,13 +10,34 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> authenticate(String email, String password) async {
     final url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
             Api.apiKey;
     final response = await http.post(url,
         body: json.encode(
             {'email': email, 'password': password, 'returnSecureToken': true}));
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+            Api.apiKey;
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true
+          }));
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+      print(responseData);
+    } catch (e) {
+      throw e;
+    }
   }
 }
