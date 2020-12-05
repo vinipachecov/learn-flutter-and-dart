015351e37a97641e6d454e6e0f15ef8eeea9a6f4
Vinicius Vieira <vinipachecov@gmail.com>
2020-12-05 11:57:55 -0300

    chore: fetch orders filtering by the user

--- shop_app/lib/providers/orders.dart
+++ shop_app/lib/providers/orders.dart
@@ -22,13 +22,17 @@ class OrderItem {
 
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
+  final String authToken;
+  final String userId;
 
   List<OrderItem> get orders {
     return [..._orders];
   }
 
+  Orders(this.authToken, this.userId, this._orders);
+
   Future<void> fetchAndSetOrders() async {
-    final url = Api.url + '/orders.json';
+    final url = Api.url + '/orders/$userId.json' + '?auth=' + this.authToken;
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -53,7 +57,7 @@ class Orders with ChangeNotifier {
   }
 
   void addOrder(List<CartItem> carProducts, double total) async {
-    final url = Api.url + '/orders.json';
+    final url = Api.url + '/orders/$userId.json' + '?auth=' + this.authToken;
     final timestamp = DateTime.now();
     final response = await http.post(url,
         body: json.encode({
