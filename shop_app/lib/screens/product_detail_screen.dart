6ccb22d5133fba1c43946a09c84c4f7499f15e2c
Vinicius Vieira <vinipachecov@gmail.com>
2020-12-09 08:08:36 -0300

    chore: add Scroll down effect in product details

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -11,15 +11,14 @@ class ProductDetailScreen extends StatelessWidget {
     final loadedProduct =
         Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
-        appBar: AppBar(
-          title: Text(loadedProduct.title),
-        ),
-        body: SingleChildScrollView(
-            child: Column(children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Hero(
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: 300,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(loadedProduct.title),
+            background: Hero(
               tag: loadedProduct.id,
               child: Image.network(
                 loadedProduct.imageUrl,
@@ -27,12 +26,17 @@ class ProductDetailScreen extends StatelessWidget {
               ),
             ),
           ),
+        ),
+        // ListView apart of multiple ListViews, so it becomes scrollable independently.
+        SliverList(
+            delegate: SliverChildListDelegate([
           SizedBox(
             height: 10,
           ),
           Text(
             '\$${loadedProduct.price}',
             style: TextStyle(color: Colors.grey, fontSize: 20),
+            textAlign: TextAlign.center,
           ),
           SizedBox(
             height: 10,
@@ -44,7 +48,12 @@ class ProductDetailScreen extends StatelessWidget {
               textAlign: TextAlign.center,
               softWrap: true,
             ),
+          ),
+          SizedBox(
+            height: 800,
           )
-        ])));
+        ])),
+      ],
+    ));
   }
 }
