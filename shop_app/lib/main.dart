547873536467e5f96a4aa9d8a08bd68efd3e453b
Vinicius Vieira <vinipachecov@gmail.com>
2020-12-09 08:29:25 -0300

    feat: added custom transition to all screens and both ios and android

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/helpers/custom_route.dart';
 import 'package:shop_app/providers/auth.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
@@ -45,6 +46,11 @@ class MyApp extends StatelessWidget {
                     primarySwatch: Colors.blue,
                     accentColor: Colors.deepOrange,
                     fontFamily: 'Lato',
+                    //  HERE CHANGES TRANSITIONS FOR ALL SCREENS
+                    // pageTransitionsTheme: PageTransitionsTheme(builders: {
+                    //   TargetPlatform.android: CustomPageTransitionBuilder(),
+                    //   TargetPlatform.iOS: CustomPageTransitionBuilder()
+                    // })
                   ),
                   home: authData.isAuth
                       ? ProductsOverviewScreen()
